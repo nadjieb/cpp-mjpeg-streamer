1d6f2476c2141471841ce9b5e99aede73b08d4e5
Muhammad Kamal Nadjieb <kamal.nadjieb@gmail.com>
2020-07-25 13:14:00 +0700

    debug test 5

--- test/src/unit-streamer.cpp
+++ test/src/unit-streamer.cpp
@@ -45,60 +45,60 @@ TEST_SUITE("streamer")
         }
     }
 
-    // TEST_CASE("publish image stream")
-    // {
-    //     GIVEN("A streamer publishing two different buffers")
-    //     {
-    //         const std::string buffer1 = "buffer1";
-    //         const std::string buffer2 = "buffer2";
+    TEST_CASE("publish image stream")
+    {
+        GIVEN("A streamer publishing two different buffers")
+        {
+            const std::string buffer1 = "buffer1";
+            const std::string buffer2 = "buffer2";
 
-    //         auto task = std::async(std::launch::async, [&]() {
-    //             nadjieb::MJPEGStreamer streamer;
+            auto task = std::async(std::launch::async, [&]() {
+                nadjieb::MJPEGStreamer streamer;
 
-    //             streamer.start(1235);
+                streamer.start(1235);
 
-    //             while (streamer.isAlive())
-    //             {
-    //                 std::this_thread::sleep_for(std::chrono::seconds(2));
-    //                 streamer.publish("/buffer1", buffer1);
-    //                 std::this_thread::sleep_for(std::chrono::seconds(2));
-    //                 streamer.publish("/buffer2", buffer2);
-    //                 streamer.stop();
-    //             }
-    //         });
+                while (streamer.isAlive())
+                {
+                    std::this_thread::sleep_for(std::chrono::seconds(2));
+                    streamer.publish("/buffer1", buffer1);
+                    std::this_thread::sleep_for(std::chrono::seconds(2));
+                    streamer.publish("/buffer2", buffer2);
+                    streamer.stop();
+                }
+            });
 
-    //         WHEN("A client request image streams")
-    //         {
-    //             httplib::Client cli("localhost", 1235);
+            WHEN("A client request image streams")
+            {
+                httplib::Client cli("localhost", 1235);
 
-    //             std::string body1;
-    //             cli.set_read_timeout(5, 0);
-    //             auto res1 = cli.Get("/buffer1", [&](const char *data, size_t data_length) {
-    //                 body1.assign(data, data_length);
-    //                 return false;
-    //             });
+                std::string body1;
+                cli.set_read_timeout(5, 0);
+                auto res1 = cli.Get("/buffer1", [&](const char *data, size_t data_length) {
+                    body1.assign(data, data_length);
+                    return false;
+                });
 
-    //             std::string body2;
-    //             cli.set_read_timeout(5, 0);
-    //             auto res2 = cli.Get("/buffer2", [&](const char *data, size_t data_length) {
-    //                 body2.assign(data, data_length);
-    //                 return false;
-    //             });
+                std::string body2;
+                cli.set_read_timeout(5, 0);
+                auto res2 = cli.Get("/buffer2", [&](const char *data, size_t data_length) {
+                    body2.assign(data, data_length);
+                    return false;
+                });
 
-    //             task.wait();
+                task.wait();
 
-    //             THEN("The received buffers equal to the initial buffers")
-    //             {
-    //                 std::string delimiter = "\r\n\r\n";
-    //                 std::string received_buffer1 = body1.substr(body1.find(delimiter) + delimiter.size());
-    //                 std::string received_buffer2 = body2.substr(body2.find(delimiter) + delimiter.size());
+                THEN("The received buffers equal to the initial buffers")
+                {
+                    std::string delimiter = "\r\n\r\n";
+                    std::string received_buffer1 = body1.substr(body1.find(delimiter) + delimiter.size());
+                    std::string received_buffer2 = body2.substr(body2.find(delimiter) + delimiter.size());
 
-    //                 CHECK(received_buffer1 == buffer1);
-    //                 CHECK(received_buffer2 == buffer2);
-    //             }
-    //         }
-    //     }
-    // }
+                    CHECK(received_buffer1 == buffer1);
+                    CHECK(received_buffer2 == buffer2);
+                }
+            }
+        }
+    }
 
     TEST_CASE("Graceful Shutdown")
     {
