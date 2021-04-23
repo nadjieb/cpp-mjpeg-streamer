9118dcb3ef51822ad531dd0e0350cba7288d3032
Muhammad Kamal Nadjieb <kamal.nadjieb@gmail.com>
2021-04-23 23:36:43 +0700

    not use filterbr

--- thirdparty/imapdl/update.sh
+++ thirdparty/imapdl/update.sh
@@ -2,3 +2,5 @@
 
 wget -N https://raw.githubusercontent.com/gsauthof/imapdl/master/COPYING
 wget -N https://raw.githubusercontent.com/gsauthof/imapdl/master/ci/filterbr.py
+
+# reference: https://stackoverflow.com/a/43726240/266378
