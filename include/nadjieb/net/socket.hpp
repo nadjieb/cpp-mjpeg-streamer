b90368f1f33327c3c103bde39fe9f0a06cb45ed9
Muhammad Kamal Nadjieb <kamal.nadjieb@gmail.com>
2022-03-03 22:10:58 +0700

    test new github workflow

--- include/nadjieb/net/socket.hpp
+++ include/nadjieb/net/socket.hpp
@@ -3,6 +3,7 @@
 #include <nadjieb/utils/platform.hpp>
 
 #ifdef NADJIEB_MJPEG_STREAMER_PLATFORM_WINDOWS
+#undef UNICODE
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <winsock2.h>
--- single_include/nadjieb/mjpeg_streamer.hpp
+++ single_include/nadjieb/mjpeg_streamer.hpp
@@ -131,6 +131,7 @@ struct HTTPMessage {
 
 
 #ifdef NADJIEB_MJPEG_STREAMER_PLATFORM_WINDOWS
+#undef UNICODE
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <winsock2.h>
