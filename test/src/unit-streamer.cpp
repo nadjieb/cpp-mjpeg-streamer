7aeed618c3306368a673dbf49b66a9a10a4757f8
Muhammad Kamal Nadjieb <kamal.nadjieb@gmail.com>
2022-03-05 00:54:04 +0700

    increase waittime in unit-test

--- test/src/unit-streamer.cpp
+++ test/src/unit-streamer.cpp
@@ -77,7 +77,7 @@ TEST_SUITE("streamer") {
 
                 while (!stop1 || !stop2) {
                     std::cout << stop1 << ' ' << stop2 << std::endl;
-                    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
                 }
 
                 std::cout << received_buffer1 << std::endl;
